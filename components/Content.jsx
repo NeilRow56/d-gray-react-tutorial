37c0e5345c424802bcc206f84036678e08f71542
Neil Rowland <neilrowland56@yahoo.com>
2022-06-23 16:58:05 +0100

    handleClick

--- components/Content.jsx
+++ components/Content.jsx
@@ -8,11 +8,37 @@ const Content = () => {
         const int = Math.floor(Math.random( ) * 3)
         return names[int]
       }
+
+    const handleClick= () => {
+        console.log('You clicked it')
+    }
+
+    const handleClick2= (name) => {
+        console.log(`${name} was clicked`)
+    }
+
+    const handleClick3= (e) => {
+        console.log(e.target.innerText)
+    }
       
   return (
     <Layout>
     <main className='flex flex-col w-full items-center justify-center'>
-        <p>Hello {handleNameChange()}</p>
+        <p
+        onDoubleClick={handleClick}
+        >Hello {handleNameChange()}</p>
+        <button
+        className='primary-button mb-4'
+        onClick={handleClick}
+        >Click It</button>
+        <button
+        className='primary-button mb-4'
+        onClick={() => handleClick2('Dave')}
+        >Click It</button>
+        <button
+        className='primary-button'
+        onClick={(e) => handleClick3(e)}
+        >Click It</button>
     </main>
     </Layout>
   )
