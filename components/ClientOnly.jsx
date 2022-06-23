9326ff7696248f3a91b03330df400137922dd443
Neil Rowland <neilrowland56@yahoo.com>
2022-06-23 14:08:43 +0100

    Initial commit - ClientOnly component- serverside rendering

---
+++ components/ClientOnly.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+
+export default function ClientOnly({ children, ...delegated }) {
+    const [hasMounted, setHasMounted] = useState(false);
+    React.useEffect(() => {
+      setHasMounted(true);
+    }, []);
+    if (!hasMounted) {
+      return null;
+    }
+    return (
+      <div {...delegated}>
+        {children}
+      </div>
+    );
+  }
